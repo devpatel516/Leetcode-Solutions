cae9158c7515f1a63e020a34a0406fe3ff5b6e36
unknown <devpatel17207@gmail.com>
2024-08-01 16:27:30 +0000

    Code challenge submission for question: 2011 Final Value of Variable After Performing Operations

---
+++ 2011 Final Value of Variable After Performing Operations/2011final-value-of-variable-after-performing-operations.c
@@ -0,0 +1,19 @@
+int finalValueAfterOperations(char** operations, int operationsSize) {
+    int x=0;
+    for(int i=0;i<operationsSize;i++){
+    if(strcmp(operations[i],"++x")==0 || strcmp(operations[i],"++X")==0){
+        ++x;
+    }
+    else if(strcmp(operations[i],"--x")==0 || strcmp(operations[i],"--X")==0){
+        --x;
+    }
+    else if(strcmp(operations[i],"x++")==0 || strcmp(operations[i],"X++")==0){
+        x++;
+    }
+    
+    else if(strcmp(operations[i],"x--")==0 || strcmp(operations[i],"X--")==0){
+        x--;
+    }
+    }
+    return x;
+}
