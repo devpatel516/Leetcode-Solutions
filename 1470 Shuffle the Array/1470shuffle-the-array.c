4ad839a68377e0376b368c9e9c1f6f9f8d9c98f5
unknown <devpatel17207@gmail.com>
2024-08-02 13:30:29 +0000

    Code challenge submission for question: 1470 Shuffle the Array

---
+++ 1470 Shuffle the Array/1470shuffle-the-array.c
@@ -0,0 +1,13 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* shuffle(int* nums, int numsSize, int n, int* returnSize) {
+    *returnSize = numsSize;
+    int *ans = (int *)malloc(*returnSize * sizeof(int));
+    for (int i = 0; i < n; i++) {
+        ans[2*i] = nums[i];       
+        ans[2*i + 1] = nums[n + i]; 
+        }
+    
+    return ans;  
+}
