53f795f9860ec8986e8e08003d64c48f6f342947
unknown <devpatel17207@gmail.com>
2025-01-06 13:59:49 +0000

    Code challenge submission for question: 53 Maximum Subarray

---
+++ 53 Maximum Subarray/53maximum-subarray.java
@@ -0,0 +1,16 @@
+//kadane's algorithm
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int sum=0,m=nums[0];
+        for(int i=0;i<nums.length;i++){
+            sum+=nums[i];
+            if(sum>m){
+                m=sum;
+            }
+            if(sum<0){
+                sum=0;
+            }
+        }
+        return m;       
+    }
+}
