d809752f627e8e19c5c9293480d2f72cc8d96718
unknown <devpatel17207@gmail.com>
2025-01-11 15:02:10 +0000

    Code challenge submission for question: 119 Pascal's Triangle II

---
+++ 119 Pascal's Triangle II/119pascals-triangle-ii.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> getRow(int rowIndex) {
+        long long ans=1;
+        vector<int> res;
+        res.push_back(1);
+        for(int i=1;i<rowIndex+1;i++){
+            ans=ans*(rowIndex-i+1);
+            ans=ans/i;
+            res.push_back(ans);
+        }
+        return res;
+    }
+};
