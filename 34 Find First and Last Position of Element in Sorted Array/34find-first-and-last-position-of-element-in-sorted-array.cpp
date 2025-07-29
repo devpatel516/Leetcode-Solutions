4a1293df268c8b92dedb677a3796108639d161de
unknown <devpatel17207@gmail.com>
2025-07-29 10:53:57 +0000

    Code challenge submission for question: 34 Find First and Last Position of Element in Sorted Array

---
+++ 34 Find First and Last Position of Element in Sorted Array/34find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int lb=lower_bound(nums.begin(),nums.end(),target)-nums.begin();
+        int ub=upper_bound(nums.begin(),nums.end(),target)-nums.begin();
+
+        if(lb==nums.size() || nums[lb]!=target){
+            return {-1,-1};
+        }
+        else{
+            if(nums[ub-1]==target) return {lb,ub-1};
+            else return {-1,-1};
+        }
+    }
+};
