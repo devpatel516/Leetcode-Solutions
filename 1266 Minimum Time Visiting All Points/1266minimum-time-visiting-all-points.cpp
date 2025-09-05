4c391c4719d4a30e3d9fc6a6a612faa44a4affdc
unknown <devpatel17207@gmail.com>
2025-09-05 15:47:54 +0000

    Code challenge submission for question: 1266 Minimum Time Visiting All Points

---
+++ 1266 Minimum Time Visiting All Points/1266minimum-time-visiting-all-points.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int minTimeToVisitAllPoints(vector<vector<int>>& points) {
+        int ans=0;
+        for(int i=1;i<points.size();i++){
+            int xdiff=abs(points[i-1][0]-points[i][0]);
+            int ydiff=abs(points[i-1][1]-points[i][1]);
+            ans+=max(xdiff,ydiff);
+        }
+        return ans;
+    }
+};
