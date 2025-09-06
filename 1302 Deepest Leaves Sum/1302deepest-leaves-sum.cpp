2562bbd1b81b8dadd32b166161aa2d622968d98d
unknown <devpatel17207@gmail.com>
2025-09-06 10:08:18 +0000

    Code challenge submission for question: 1302 Deepest Leaves Sum

---
+++ 1302 Deepest Leaves Sum/1302deepest-leaves-sum.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int deepestLeavesSum(TreeNode* root) {
+        queue<TreeNode*> q;
+        q.push(root);
+        int s;
+        while(!q.empty()){
+            int size=q.size();
+            s=0;
+            for(int i=0;i<size;i++){
+                int a=q.front()->val;
+                s+=a;
+                if(q.front()->left){
+                    q.push(q.front()->left);
+                }
+                if(q.front()->right){
+                    q.push(q.front()->right);
+                }
+                q.pop();
+            }
+        }
+        return s;
+    }
+};
