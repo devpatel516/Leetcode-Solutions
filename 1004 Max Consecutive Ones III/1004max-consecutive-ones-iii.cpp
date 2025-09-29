96120ce1f19b1ef884b8bb089188f4f45038ebc5
unknown <devpatel17207@gmail.com>
2025-09-29 08:54:24 +0000

    Code challenge submission for question: 1004 Max Consecutive Ones III

---
+++ 1004 Max Consecutive Ones III/1004max-consecutive-ones-iii.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int longestOnes(vector<int>& nums, int k) {
+        int l=0;
+        int r=0;
+        int zeros=0;
+        int n=nums.size();
+        int ans=0;
+        while(r<n){
+            if(nums[r]==0){
+                zeros++;
+            }
+            if(zeros>k){
+                if(nums[l]==0){
+                    zeros--;
+                }
+                l++;
+            }
+            if(zeros<=k){
+                ans=max(ans,r-l+1);
+            }
+            r++;
+        }
+        return ans;
+    }
+};
