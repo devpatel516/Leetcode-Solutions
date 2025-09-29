6416069ab65b156280320d10629ebd062d53b361
unknown <devpatel17207@gmail.com>
2025-09-29 13:13:59 +0000

    Code challenge submission for question: 1423 Maximum Points You Can Obtain from Cards

---
+++ 1423 Maximum Points You Can Obtain from Cards/1423maximum-points-you-can-obtain-from-cards.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maxScore(vector<int>& cardPoints, int k) {
+        int ans=0;
+        int lsum=0,rsum=0;
+
+        for(int i=0;i<k;i++){
+            lsum+=cardPoints[i];
+        }
+        ans=lsum;
+        int r=cardPoints.size()-1;
+        for(int i=k-1;i>=0;i--){
+            lsum-=cardPoints[i];
+            rsum+=cardPoints[r];
+            r--;
+            ans=max(ans,lsum+rsum);
+        }
+        return ans;
+    }
+};
