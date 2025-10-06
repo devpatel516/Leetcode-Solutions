4b68829df91a919db84a29c579e64cba1e3c0a1c
unknown <devpatel17207@gmail.com>
2025-10-06 17:03:11 +0000

    Code challenge submission for question: 342 Power of Four

---
+++ 342 Power of Four/342power-of-four.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isPowerOfFour(int n) {
+        if(n<=0) return false;
+        if(((1LL*n) & (n-1))==0 && n%3==1){
+            return true;
+        }
+        return false;
+    }
+};
+
+
