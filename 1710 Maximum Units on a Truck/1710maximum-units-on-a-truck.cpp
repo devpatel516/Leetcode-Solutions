a6b65828da6363e4c3b4e3a382c18286676e382e
unknown <devpatel17207@gmail.com>
2025-10-08 13:58:57 +0000

    Code challenge submission for question: 1710 Maximum Units on a Truck

---
+++ 1710 Maximum Units on a Truck/1710maximum-units-on-a-truck.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int maximumUnits(vector<vector<int>>& boxTypes, int truckSize) {
+        sort(boxTypes.begin(),boxTypes.end(),[](const vector<int> a,const vector<int> b){
+             return a[1]>b[1];
+        });
+        int n=boxTypes.size();
+        int ans=0;
+        for(int i=0;i<n;i++){
+            if(boxTypes[i][0]<=truckSize){
+                ans+=boxTypes[i][0]*boxTypes[i][1];
+                truckSize-=boxTypes[i][0];
+            }
+            else if(truckSize>0){
+                ans+=truckSize*boxTypes[i][1];
+                return ans;
+            }
+        }
+        return ans;
+    }
+};
