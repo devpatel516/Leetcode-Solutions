b3b55f499666bb581a952c8331cc97888a42a4de
unknown <devpatel17207@gmail.com>
2025-10-14 16:13:05 +0000

    Code challenge submission for question: 198 House Robber

---
+++ 198 House Robber/198house-robber.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int helper(int i,vector<int>& nums,vector<int>& dp){
+        if(i==0){
+            return nums[i];
+        }
+        if(i<0){
+            return 0;
+        }
+        if(dp[i]!=-1){
+            return dp[i];
+        }
+        int rob=nums[i]+helper(i-2,nums,dp);
+        int notrob=0+helper(i-1,nums,dp);
+        return dp[i]=max(rob,notrob);
+    }
+    int rob(vector<int>& nums) {
+       int n=nums.size();
+       vector<int> dp(n,-1);
+       return helper(n-1,nums,dp); 
+    }
+};
