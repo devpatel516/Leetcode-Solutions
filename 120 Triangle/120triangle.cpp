9a3a8be538895e1531b9d00d40fc9612aeabb073
unknown <devpatel17207@gmail.com>
2025-10-18 11:27:10 +0000

    Code challenge submission for question: 120 Triangle

---
+++ 120 Triangle/120triangle.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int m=triangle.size();
+        vector<vector<int>> dp(m,vector<int>(m,-1));
+        for(int j=0;j<m;j++){
+            dp[m-1][j]=triangle[m-1][j];
+        }
+
+        for(int i=m-2;i>=0;i--){
+            for(int j=i;j>=0;j--){
+                int down=triangle[i][j]+dp[i+1][j];
+                int diag=triangle[i][j]+dp[i+1][j+1];
+
+                dp[i][j]=min(down,diag);
+            }
+        }
+
+        return dp[0][0];
+    }
+};
