acaf8d39a994fbc51212ac9bd5256c1207c3e97a
unknown <devpatel17207@gmail.com>
2025-10-30 16:36:15 +0000

    Code challenge submission for question: 33 Search in Rotated Sorted Array

---
+++ 33 Search in Rotated Sorted Array/33search-in-rotated-sorted-array.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        //7, 8, 9, 1, 2, 3, 4, 5, 6
+        int l=0;
+        int r=nums.size()-1;
+        while(l<=r){
+            int mid=(l+r)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+            if(nums[l]<=nums[mid]){
+                if(nums[l]<=target && target<=nums[mid]){
+                    r=mid-1;
+                }
+                else{
+                    l=mid+1;
+                }
+            }
+            else{
+                if(nums[mid]<=target && target<=nums[r]){
+                    l=mid+1;
+                }
+                else{
+                    r=mid-1;
+                }
+            }
+        }
+        return -1;
+    }
+};
