f09540fd077c92ca46d9fd99771a6130e2cb7ce2
unknown <devpatel17207@gmail.com>
2025-12-18 13:55:20 +0000

    Code challenge submission for question: 977 Squares of a Sorted Array

---
+++ 977 Squares of a Sorted Array/977squares-of-a-sorted-array.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+        int n=nums.size();
+        vector<int> ans(n);
+        int l=0;
+        int r=n-1;
+        int k=n-1;
+        while(l<=r){
+            if(abs(nums[l])>abs(nums[r])){
+                ans[k]=nums[l]*nums[l];
+                l++;
+                k--;
+            }
+            else{
+                ans[k]=nums[r]*nums[r];
+                r--;
+                k--;
+            }
+        }   
+        // reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
