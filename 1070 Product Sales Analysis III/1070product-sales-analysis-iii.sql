12b21070b5106ef2b8240ccc8b80b8f91ab07fc9
unknown <devpatel17207@gmail.com>
2026-05-14 13:03:37 +0000

    Code challenge submission for question: 1070 Product Sales Analysis III

---
+++ 1070 Product Sales Analysis III/1070product-sales-analysis-iii.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+with cte as(select product_id,min(year) as 'first_year' from Sales group by product_id)
+
+select s.product_id,s.year as 'first_year',s.quantity,s.price from sales s
+join cte c 
+on s.product_id=c.product_id and s.year=c.first_year;
