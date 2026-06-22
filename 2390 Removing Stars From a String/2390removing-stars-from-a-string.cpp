667524b46ba53141ab23d87e404fa44c21ff5b4f
unknown <devpatel17207@gmail.com>
2026-06-22 11:44:40 +0000

    Code challenge submission for question: 2390 Removing Stars From a String

---
+++ 2390 Removing Stars From a String/2390removing-stars-from-a-string.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string removeStars(string s) {
+        stack<char> st;
+        for (auto c : s) {
+            if (c != '*') {
+                st.push(c);
+            } else if (c == '*') {
+                if(!st.empty()) st.pop();
+            }
+        }
+        string ans="";
+        while(!st.empty()){
+            ans+=st.top();
+            st.pop();
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
