815333b29e8eee1bb8ed3ab7c28e20031f36b6b8
unknown <devpatel17207@gmail.com>
2026-06-25 12:56:51 +0000

    Code challenge submission for question: 649 Dota2 Senate

---
+++ 649 Dota2 Senate/649dota2-senate.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string predictPartyVictory(string senate) {
+       int n=senate.size();
+       queue<int> radient,dire;
+       for(int i=0;i<n;i++){
+        if(senate[i]=='R'){
+            radient.push(i);
+        }else{
+            dire.push(i);
+        }
+       }
+
+       while(!radient.empty() && !dire.empty()){
+        int r=radient.front();
+        radient.pop();
+        int d=dire.front();
+        dire.pop();
+        if(r<d){
+            radient.push(r+n);
+        }else{
+            dire.push(d+n);
+        }
+       }
+       return radient.empty()?"Dire":"Radiant";
+    }
+};
