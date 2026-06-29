07e28328d839658c27cdcb85cd81912d4d313a0e
unknown <devpatel17207@gmail.com>
2026-06-29 05:15:38 +0000

    Code challenge submission for question: 1846 Maximum Element After Decreasing and Rearranging

---
+++ 1846 Maximum Element After Decreasing and Rearranging/1846maximum-element-after-decreasing-and-rearranging.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maximumElementAfterDecrementingAndRearranging(vector<int>& arr) {
+        if(arr.size()==1){
+            return 1;
+        }
+        sort(arr.begin(),arr.end());
+        arr[0]=1;
+        for(int i=1;i<arr.size();i++){
+            if(abs(arr[i]-arr[i-1])<=1){
+                continue;
+            }else{
+                arr[i]=arr[i-1]+1;
+            }
+        }
+        return arr.back();
+    }
+};
