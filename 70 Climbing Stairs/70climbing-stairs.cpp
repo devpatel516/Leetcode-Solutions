a1aa4b68108c1760dd802b92e5180aaac62972f4
unknown <devpatel17207@gmail.com>
2026-07-03 12:23:31 +0000

    Code challenge submission for question: 70 Climbing Stairs

---
+++ 70 Climbing Stairs/70climbing-stairs.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> dp = vector<int>(50, -1); 
+    int climbStairs(int n) {
+        if(n==0){
+            return 1;
+        }
+        if(n==1){
+            return 1;
+        }
+        if(dp[n]!=-1){
+            return dp[n];
+        }
+        return dp[n]=climbStairs(n-1)+climbStairs(n-2);
+    }
+};
