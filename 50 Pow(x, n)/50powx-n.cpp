d7e764a5a878ea75306fecfef2c4e6a654130024
unknown <devpatel17207@gmail.com>
2026-07-08 14:09:24 +0000

    Code challenge submission for question: 50 Pow(x, n)

---
+++ 50 Pow(x, n)/50powx-n.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    double solve(double x,int n){
+       if(n==0){
+            return 1;
+       } 
+       double half=solve(x,n/2);
+       if(n%2==0){
+            return half*half;
+       }
+       return x*half*half;
+    }
+    double myPow(double x, int n) {
+       if(n<0){
+            return 1/solve(x,n);
+       }
+       return solve(x,n);
+    }
+};
